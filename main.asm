03d29952711da8a2487b2424914b287ad383972d
Sandingo <78052914+Sandingo@users.noreply.github.com>
2025-02-12 20:28:57 -0600

    Minor Sprite Edits

--- main.asm
+++ main.asm
@@ -356,8 +356,8 @@ INCLUDE "gfx/fishing.asm"
 INCLUDE "data/moves/animations.asm"
 INCLUDE "data/battle_anims/subanimations.asm"
 INCLUDE "data/battle_anims/frame_blocks.asm"
-INCLUDE "engine/movie/evolution.asm"
 
 SECTION "bank1F", ROMX
 
+INCLUDE "engine/movie/evolution.asm"
 INCLUDE "engine/overworld/elevator.asm"
